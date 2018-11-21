b23672ed888cbdf273ae80ac07a918eb8e04935a
mmmaaaggg <mmmaaaggg@163.com>
2018-11-21 16:22:54 +0800

    support multi traders

--- ibats_main/run.py
+++ ibats_main/run.py
@@ -19,13 +19,12 @@ if os.path.abspath(os.curdir) not in sys.path:
 
 from ibats_main.config import config
 from ibats_common.common import PeriodType, RunMode, BacktestTradeMode, ExchangeName
-from ibats_common.strategy import StgHandlerBase, strategy_handler_factory
-from ibats_main.strategy.bs_against_files.csv_orders import ReadFileStg
+from ibats_common.strategy_handler import strategy_handler_factory
 from ibats_main.strategy.simple_strategy import MACroseStg
 
 logger = logging.getLogger()
 
-strategy_list = [ReadFileStg, MACroseStg]  #
+strategy_list = [MACroseStg]  #
 promt_str = '输入对应数字选择执行策略：\n' + \
             '\n'.join(['%d) %s' % (num, foo.__name__) for num, foo in enumerate(strategy_list, start=1)]) + '\n'
 
@@ -48,33 +47,37 @@ def run_strategy(num):
 
     DEBUG = False
     # 参数设置
-    # 参数设置
-    strategy_params = {'unit': 100000}
+    run_mode = RunMode.Backtest
+    strategy_params = {'unit': 100}
     md_agent_params_list = [{
         'md_period': PeriodType.Min1,
-        'instrument_id_list': ['ETHUSD'],
-        'init_md_date_from': '2018-7-19',
+        'instrument_id_set': {'ETHUSD'},
+        'init_md_date_from': '2018-7-19',  # 行情初始化加载历史数据，供策略分析预加载使用
         'init_md_date_to': '2018-10-21',
     }]
-    trade_agent_params_realtime = {
-        'run_mode': RunMode.Realtime,
-    }
-    trade_agent_params_backtest = {
-        'run_mode': RunMode.Backtest,
-        'date_from': '2018-7-19',
-        'date_to': '2018-7-20',
-        'init_cash': 1000000,
-        'trade_mode': BacktestTradeMode.Order_2_Deal
-    }
-    # run_mode = RunMode.BackTest
+    if run_mode == RunMode.Realtime:
+        trade_agent_params = {
+        }
+        strategy_handler_param = {
+        }
+    else:
+        trade_agent_params = {
+            'trade_mode': BacktestTradeMode.Order_2_Deal,
+            'init_cash': 1000000,
+        }
+        strategy_handler_param = {
+            'date_from': '2018-7-19',  # 策略回测历史数据，回测指定时间段的历史行情
+            'date_to': '2018-7-20',
+        }
     # 初始化策略处理器
     stghandler = strategy_handler_factory(
         stg_class=stg_func,
         strategy_params=strategy_params,
         md_agent_params_list=md_agent_params_list,
         exchange_name=ExchangeName.BitMex,
-        **trade_agent_params_backtest,
-        # **run_mode_params_realtime,
+        run_mode=RunMode.Backtest,
+        trade_agent_params=trade_agent_params,
+        strategy_handler_param=strategy_handler_param,
     )
 
     if DEBUG:
--- ibats_main/strategy/abitrage_stg.py
+++ ibats_main/strategy/abitrage_stg.py
@@ -2,36 +2,29 @@
 # -*- coding:utf-8 -*-
 """
 @author  : MG
-@Time    : 2018/6/21 11:26
-@File    : simple_strategy.py
+@Time    : 2018/11/7 10:42
+@File    : ma_cross_stg.py
 @contact : mmmaaaggg@163.com
 @desc    : 
 """
-import logging
 import time
-import pandas as pd
-from ibats_main.config import config  # 单策略执行时，该语句需写在加载 ibats_common 模块前面，以便首先更新数据库配置信息
-from ibats_common.common import BacktestTradeMode, PeriodType, RunMode, ContextKey, Direction, ExchangeName
+import logging
+from ibats_common.common import PeriodType, RunMode, BacktestTradeMode, ExchangeName, ContextKey, Direction
 from ibats_common.strategy import StgBase
-from ibats_common.strategy_handler import strategy_handler_factory
-# 下面代码是必要的引用
-# md_agent md_agent 并没有“显式”的被使用，但是在被引用期间，已经将相应的 agent 类注册到了相应的列表中
-import ibats_bitmex_trader.agent.md_agent
-import ibats_bitmex_trader.agent.td_agent
+from ibats_common.strategy_handler import strategy_handler_factory_multi_exchange
 
 logger = logging.getLogger(__name__)
 
 
-class MACroseStg(StgBase):
+class MACrossStg(StgBase):
 
-    def __init__(self, unit=1):
+    def __init__(self):
         super().__init__()
         self.ma5 = []
         self.ma10 = []
-        self.unit = unit
 
-    def on_prepare_min1(self, md_df: pd.DataFrame, context):
-        if md_df is not None and md_df.shape[0] > 0:
+    def on_prepare_min1(self, md_df, context):
+        if md_df:
             self.ma5 = list(md_df['close'].rolling(5, 5).mean())[10:]
             self.ma10 = list(md_df['close'].rolling(10, 10).mean())[10:]
 
@@ -51,7 +44,7 @@ class MACroseStg(StgBase):
                     elif direction == Direction.Long:
                         no_target_position = False
             if no_target_position:
-                self.open_long(instrument_id, close, self.unit)
+                self.open_long(instrument_id, close, 1)
         elif self.ma5[-2] > self.ma10[-2] and self.ma5[-1] < self.ma10[-1]:
             position_date_pos_info_dic = self.get_position(instrument_id)
             no_target_position = True
@@ -63,45 +56,66 @@ class MACroseStg(StgBase):
                     elif direction == Direction.Short:
                         no_target_position = False
             if no_target_position:
-                self.open_short(instrument_id, close, self.unit)
+                self.open_short(instrument_id, close, 1)
 
 
-if __name__ == '__main__':
+def _test_use():
     # 参数设置
     run_mode = RunMode.Backtest
-    strategy_params = {'unit': 100}
-    md_agent_params_list = [{
-        'md_period': PeriodType.Min1,
-        'instrument_id_list': ['ETHUSD'],
-        'init_md_date_from': '2018-7-19',  # 行情初始化加载历史数据，供策略分析预加载使用
-        'init_md_date_to': '2018-10-21',
-    }]
-    if run_mode == RunMode.Realtime:
-        trade_agent_params = {
+    strategy_params = {}
+    md_agent_params_list = [
+        {
+            'md_period': PeriodType.Min1,
+            'exchange_name': ExchangeName.BitMex,
+            'instrument_id_set': {'ETHUSD'},  # ['jm1711', 'rb1712', 'pb1801', 'IF1710'],
+            'init_md_date_to': '2017-9-1',
+        },
+        {
+            'md_period': PeriodType.Min1,
+            'exchange_name': ExchangeName.HuoBi,
+            'instrument_id_set': {'ETHUSD'},  # ['jm1711', 'rb1712', 'pb1801', 'IF1710'],
+            'init_md_date_to': '2017-9-1',
         }
+    ]
+    if run_mode == RunMode.Realtime:
+        trade_agent_params_list = [{
+        }]
         strategy_handler_param = {
         }
     else:
-        trade_agent_params = {
-            'trade_mode': BacktestTradeMode.Order_2_Deal,
-            'init_cash': 1000000,
-        }
+        trade_agent_params_list = [
+            {
+                'exchange_name': ExchangeName.BitMex,
+                'trade_mode': BacktestTradeMode.Order_2_Deal,
+                'init_cash': 1000000,
+            },
+            {
+                'exchange_name': ExchangeName.BitMex,
+                'trade_mode': BacktestTradeMode.Order_2_Deal,
+                'init_cash': 1000000,
+            }
+        ]
         strategy_handler_param = {
             'date_from': '2018-7-19',  # 策略回测历史数据，回测指定时间段的历史行情
             'date_to': '2018-7-20',
         }
-    # run_mode = RunMode.BackTest
+
     # 初始化策略处理器
-    stghandler = strategy_handler_factory(stg_class=MACroseStg,
-                                          strategy_params=strategy_params,
-                                          md_agent_params_list=md_agent_params_list,
-                                          exchange_name=ExchangeName.BitMex,
-                                          run_mode=RunMode.Backtest,
-                                          trade_agent_params=trade_agent_params,
-                                          strategy_handler_param=strategy_handler_param,
-                                          )
+    stghandler = strategy_handler_factory_multi_exchange(
+        stg_class=MACrossStg,
+        strategy_params=strategy_params,
+        md_agent_params_list=md_agent_params_list,
+        run_mode=RunMode.Backtest,
+        trade_agent_params_list=trade_agent_params_list,
+        strategy_handler_param=strategy_handler_param,
+    )
     stghandler.start()
     time.sleep(10)
     stghandler.keep_running = False
     stghandler.join()
     logging.info("执行结束")
+
+
+if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG, format=config.LOG_FORMAT)
+    _test_use()
